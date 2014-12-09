4bf165e72a4e9fff446349b60ede2fee512c4487
owen-d <ow.diehl@gmail.com>
2014-12-08 16:27:40 -0800

    working snow-like particles

---
+++ ParticleTest.js
@@ -0,0 +1,11 @@
+// var pointLight =
+  //   new THREE.PointLight(0xFFFFFF);
+
+  // // set its position
+  // pointLight.position.x = 10;
+  // pointLight.position.y = 50;
+  // pointLight.position.z = 130;
+
+  // // add to the scene
+  // // console.log('scene:', scene);
+  // scene.add(pointLight);
